35ecf52ab810fb3c2cc4fe9efb4cf0bb18126371
mucosmos <cedric.vanden-driessche@ac-normandie.fr>
2025-02-05 20:43:33 +0100

    Update MicroCosmosDetectionCosmiques.ino

--- MicroCosmosDetectionCosmiques.ino
+++ MicroCosmosDetectionCosmiques.ino
@@ -1,9 +1,9 @@
 // Programme de comptage de coïncidences entre 2 tubes Geiger. Prévu pour Arduino Uno.
 // Auteur : Cédric Vanden Driessche, Lycée Charles de Gaulle, CAEN. mél : cedric.vanden-driessche@ac-normandie.fr
 // copyright : cédric vanden driessche , 2024
-//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//You should have received a copy of the GNU General Public License along with this program
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program
 
 long Tinit;                 // Définition des variables
 long duree;
